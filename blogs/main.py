db6477f3061e4600cafd55c27870810fc735edab
Abdullah Alharbi <alharbi.s.abdullah@gmail.com>
2022-10-28 18:09:43 +0300

    added code.

--- blogs/main.py
+++ blogs/main.py
@@ -1,14 +1,17 @@
-from fastapi import FastAPI , Depends
+from fastapi import (FastAPI , Depends , status ,
+                     Response) 
 from . import schemas, models
 from .database import engine , get_db
 from sqlalchemy.orm import Session
 
-
+# creating the app 
 app = FastAPI()
 
+# creating database schemas and tables.
 models.Base.metadata.create_all(engine)
 
-@app.post('/blog')
+# post new blogs 
+@app.post('/blog', status_code= status.HTTP_201_CREATED)
 def create(request: schemas.Blog , db: Session = Depends(get_db)):
     new_blog = models.Blog(title=request.title , 
                             body=request.body)
@@ -18,3 +21,25 @@ def create(request: schemas.Blog , db: Session = Depends(get_db)):
     return {
         'new blog created' : new_blog
     }
+
+# get blogs 
+@app.get('/blog')
+def get_blogs(db: Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return {
+        'blogs' : blogs
+    }
+
+# get a blog by ID 
+@app.get('/blog/{id}' , status_code= status.HTTP_200_OK)
+def get_blog(id , response: Response, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {
+            'details' : f"blog with ID {id} doesn't exists"
+        }
+    return {
+        f'blog for ID {id}' : blog
+    }
+
